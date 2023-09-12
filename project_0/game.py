acf0960ab0790f702e5911625c048611a7682f64
AlekseyTau <144460924+AlekseyTau@users.noreply.github.com>
2023-09-12 11:34:39 +0200

    Add files via upload

---
+++ project_0/game.py
@@ -0,0 +1,20 @@
+import numpy as np
+number = np.random.randint(1, 101) # set the number
+
+# attempt count
+count = 0
+while True:
+    
+    count += 1
+    predict_number = int(input('Guess the number from 1 to 100:'))
+    
+    if predict_number > number:
+        print('The number should be less')
+    elif predict_number < number:
+        print('The number should be bigger')
+    else:
+        print(f"Right guess! It's {number} Congratulations! It took you {count} attempts!")
+        break # Stop if guessed right
+    
+'Just wanna scream!!!'
+    
